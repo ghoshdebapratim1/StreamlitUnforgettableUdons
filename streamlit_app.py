50462aa2f5a6808cc0a9396739db528099957a59
ghoshdebapratim1 <89624268+ghoshdebapratim1@users.noreply.github.com>
2023-07-13 15:07:21 +0000

    Added questions to be visualised

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,34 @@ st.write(df.head())
 st.header('Section 1 - Data Pre Processing ')
 
 
+
+
+
+
+
+st.header('Section 1 - Questions related to the dataset')
+
+## Obioma 
+st.subheader("which types of movie genres usually have a longer runtime? ")
+st.subheader("which movie has the biggest average rating?")
+
+## Farah 
+st.subheader("Which genre is the most common?")
+st.subheader("Does the date the movie was produced on matter in film success?")
+
+## Broderic 
+st.subheader("What is if any correlation between a movies rating and worldwide earnings?")
+st.subheader("What is, if any, the correlations between production budget and worldwide earnings?")
+
+## Gordon 
+st.subheader("Which movies have the highest gross profit?")
+st.subheader("Who are the most common directors in the film industry?")
+
+
+## You Gang 
+st.subheader('which movie genre has the most dead directors?')
+st.subheader('what are the lowest rated movies?')
+
+
+
+st.header('Conclusion')
